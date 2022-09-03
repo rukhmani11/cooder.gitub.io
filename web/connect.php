47ffc6dfa63dad2d02270cf2d60cc0fd9d339c24
rukhmani11 <112771974+rukhmani11@users.noreply.github.com>
2022-09-03 22:21:17 +0530

    food web

---
+++ web/connect.php
@@ -0,0 +1,8 @@
+<?php
+$firstname= $_POST['firstName'];
+$Email = $_POST['Email'];
+$Phone NUmber= $_POST['Phone NUmber'];
+$Password = $_POST['Password'];
+$confirm Password= $_POST['confirm Password'];
+//Databae connection
+?>
